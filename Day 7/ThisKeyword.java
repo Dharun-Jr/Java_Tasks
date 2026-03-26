4fc8a502bd000144f73b698bbfce641022ccaf35
Dharun JR <jrdharun095@gmail.com>
2026-03-26 12:45:21 +0530

    Update ThisKeyword.java

--- Day 7/ThisKeyword.java
+++ Day 7/ThisKeyword.java
@@ -1,15 +1,17 @@
 class Student{
     int roll;
     String name;
-    void display(int roll, String name){
+    public Student(int roll, String name){
         this.roll = roll;
         this.name = name;
+    }
+    void display(){
         System.out.println("Roll Num: " + roll + "\nName: " + name);
     }
 }
 public class ThisClass{
     public static void main(String[] args) {
-        Student ob = new Student();
-        ob.display(3123, "Dharun");
+        Student ob = new Student(3123, "Dharun");
+        ob.display();
     }
 }
