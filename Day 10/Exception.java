1c13e10f2e589f5df8e02d69140cc70f84b3aa4d
Dharun JR <jrdharun095@gmail.com>
2026-03-31 10:31:43 +0530

    Create Exception.java

---
+++ Day 10/Exception.java
@@ -0,0 +1,14 @@
+
+public class Err {
+    public static void main(String[] args) {
+        try{
+            int n = 10/0;
+            System.out.println(n);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("Reached the end of the code");
+        }
+
+    }
+}
