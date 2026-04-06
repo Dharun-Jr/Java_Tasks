1c151bfeab622e69bdbd5d8a7690e460c2b95f6f
Dharun JR <jrdharun095@gmail.com>
2026-04-06 13:06:57 +0530

    Create HashSet.java

---
+++ Day 13/HashSet.java
@@ -0,0 +1,13 @@
+package Set;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.*;
+public class SetImp {
+    public static void main(String[] args) {
+        Set<Integer> s = new HashSet<>();
+        s.add(1000);
+        s.add(1);
+        s.add(2);
+        System.out.println(s);
+    }
+}
